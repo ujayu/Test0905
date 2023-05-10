5c68c4c5be1cedd6961d17aeecfd5e039f5b0f40
Jayant Mohan Uttarwar <131381048+ujayu@users.noreply.github.com>
2023-05-10 08:33:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
 var express = require('express');
-var fs = require('fs');
 var path=require('path');
 var app = express();
-var _dir = fs.realpathSync('.');
-_dir = _dir+'/public/'
+
 //server configuration
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', function (req, res) {
-    res.sendFile((_dir + '/index.html'));
- });
- 
- app.get('/array', function (req, res) {
-    res.sendFile((_dir + '/Arrays.html'));
- });
- 
- app.get('/try', function (req, res) {
-    res.sendFile((_dir + '/try.html'));
+    res.sendFile(path.join(___dirname + '/index.html'));
  });
 
 
 app.get('/hello', function (req, res) {
-    var person={firstName:'Ravi',lastName:'Tambade',age:47};
+    var person={firstName:'Jayant',lastName:'uttarwar',age:47};
     res.send(person);
 });
 
